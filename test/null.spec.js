5ea9eb92dd644e8a9a273d3dd0aa200ce36badea
e1r0nd <a.merezhanyi@gmail.com>
2017-05-06 21:52:35 +0200

    Add a fake test

---
+++ test/null.spec.js
@@ -0,0 +1,10 @@
+/* Tests will be provided */
+/* global describe, it */
+
+import assert from "assert";
+
+describe("# Null test", () => {
+  it("check for nothing", () => {
+    assert(true);
+  });
+});
