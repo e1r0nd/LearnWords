f99ec42c40ffa8f99d0a6624d4959c29ca41c5a6
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2017-06-12 22:33:53 +0200

    Styled buttons

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -7,7 +7,7 @@ const Button = (props) => {
   return buttonTmp
     .replace(/{{id}}/g, `"${props.id}"` || "")
     .replace(/{{label}}/g, props.label || "")
-    .replace(/{{class}}/g, `"${props.class}"` || "")
+    .replace(/{{class}}/g, `"${props.class}"` || "btn-primary")
     .replace(/{{dataLang}}/g, `"${props.dataLang}"` || "");
 };
 
