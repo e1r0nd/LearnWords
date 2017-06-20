992d6121d9426dfd7d5b05074bcfd578d47b774f
i.merezhanyi <i.merezhanyi@gmail.com>
2017-06-20 10:33:36 -0400

    Changed Jasmine config to get tests and add note to the initial test.

--- spec/spec.js
+++ spec/spec.js
@@ -1,10 +1,12 @@
 /* Tests will be provided */
 /* global describe, it */
 
-// import assert from "assert";
+// use 'jasmine JASMINE_CONFIG_PATH=[pass to json file]/jasmine.json' to run tests
+
+// import assert from "assert"; //this string doesn't work
 
 describe("# Null test", () => {
   it("check for nothing", () => {
-    expect(true).toBe(true); //     assert(true);
+    expect(true).toBe(true); //     assert(true); //so that this string doesn't work as well
   });
 });
